9fd4003807bbe25ec4acf0da03cb02cd2062f273
mi_sawa <mi.sawa.1216+git@gmail.com>
2020-08-07 13:56:15 +0900

    Whatever I have now

--- keyboards/caravelle_ble/keymaps/misawa/constants.h
+++ keyboards/caravelle_ble/keymaps/misawa/constants.h
@@ -2,31 +2,36 @@
 #include QMK_KEYBOARD_H
 
 enum layer_ids {
-    LAYER_BASE = 0,
+    LAYER_QWERTY = 0,
+    LAYER_WORKMAN,
     LAYER_LOWER,
     LAYER_RAISE,
     LAYER_ADJUST,
 };
 
 enum custom_keycodes {
-    AD_WO_L = SAFE_RANGE, /* Start advertising without whitelist  */
-    BLE_DIS,              /* Disable BLE HID sending              */
-    BLE_EN,               /* Enable BLE HID sending               */
-    USB_DIS,              /* Disable USB HID sending              */
-    USB_EN,               /* Enable USB HID sending               */
-    DELBNDS,              /* Delete all bonding                   */
-    ADV_ID0,              /* Start advertising to PeerID 0        */
-    ADV_ID1,              /* Start advertising to PeerID 1        */
-    ADV_ID2,              /* Start advertising to PeerID 2        */
-    ADV_ID3,              /* Start advertising to PeerID 3        */
-    ADV_ID4,              /* Start advertising to PeerID 4        */
-    BATT_LV,              /* Display battery level in milli volts */
-    DEL_ID0,              /* Delete bonding of PeerID 0           */
-    DEL_ID1,              /* Delete bonding of PeerID 1           */
-    DEL_ID2,              /* Delete bonding of PeerID 2           */
-    DEL_ID3,              /* Delete bonding of PeerID 3           */
-    DEL_ID4,              /* Delete bonding of PeerID 4           */
-    ENT_DFU,              /* Start bootloader                     */
-    ENT_SLP,              /* Deep sleep mode                      */
+    KC_QWERTY = SAFE_RANGE, // Set QWERTY as the default layout
+    KC_WORKMAN,             // Set Workman as the default layout
+
+    // BLE-related
+    AD_WO_L,                // Start advertising without whitelist
+    BLE_DIS,                // Disable BLE HID sending
+    BLE_EN,                 // Enable BLE HID sending
+    USB_DIS,                // Disable USB HID sending
+    USB_EN,                 // Enable USB HID sending
+    DELBNDS,                // Delete all bonding
+    ADV_ID0,                // Start advertising to PeerID 0
+    ADV_ID1,                // Start advertising to PeerID 1
+    ADV_ID2,                // Start advertising to PeerID 2
+    ADV_ID3,                // Start advertising to PeerID 3
+    ADV_ID4,                // Start advertising to PeerID 4
+    BATT_LV,                // Display battery level in milli volts
+    DEL_ID0,                // Delete bonding of PeerID 0
+    DEL_ID1,                // Delete bonding of PeerID 1
+    DEL_ID2,                // Delete bonding of PeerID 2
+    DEL_ID3,                // Delete bonding of PeerID 3
+    DEL_ID4,                // Delete bonding of PeerID 4
+    ENT_DFU,                // Start bootloader
+    ENT_SLP,                // Deep sleep mode
 };
 
--- keyboards/caravelle_ble/keymaps/misawa/keymap.c
+++ keyboards/caravelle_ble/keymaps/misawa/keymap.c
@@ -18,17 +18,32 @@
 #define LOWERSPC LT(LAYER_LOWER, KC_SPC)
 #define RAISEENT LT(LAYER_RAISE, KC_ENT)
 
+#define QWERTY KC_QWERTY
+#define WORKMAN KC_WORKMAN
+
 #define layer_state_t uint32_t
 
 // clang-format off
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE] = LAYOUT(
+  [LAYER_QWERTY] = LAYOUT(
  //.---------+---------+---------+---------+---------+---------\                        /---------+---------+---------+---------+---------+---------.
        KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                              KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_BSLS,
  //|---------+---------+---------+---------+---------+---------+---------+    +---------+---------+---------+---------+---------+---------+---------|
-      KC_LCTL,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,  KC_LPRN,       KC_RPRN,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
+      KC_LCTL,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,  KC_LBRC,       KC_RBRC,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
  //|---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------|
-      KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_LBRC,       KC_RBRC,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
+      KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_LPRN,       KC_RPRN,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
+ //\---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------/
+                                   LALTMHEN,  LCTLDEL, KC_LOWER,   KC_SPC,        KC_ENT, KC_RAISE, RCTLBSPC, RALTHENK
+ //                              \---------+---------+---------+---------/    \---------+---------+---------+---------/
+  ),
+
+  [LAYER_WORKMAN] = LAYOUT(
+ //.---------+---------+---------+---------+---------+---------\                        /---------+---------+---------+---------+---------+---------.
+       KC_TAB,     KC_Q,     KC_D,     KC_R,     KC_W,     KC_B,                              KC_J,     KC_F,     KC_U,     KC_P,  KC_SCLN,  KC_BSLS,
+ //|---------+---------+---------+---------+---------+---------+---------+    +---------+---------+---------+---------+---------+---------+---------|
+      KC_LCTL,     KC_A,     KC_S,     KC_H,     KC_T,     KC_G,  KC_LBRC,       KC_RBRC,     KC_Y,     KC_N,     KC_E,     KC_O,     KC_I,  KC_QUOT,
+ //|---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------|
+      KC_LSFT,     KC_Z,     KC_X,     KC_M,     KC_C,     KC_V,  KC_LPRN,       KC_RPRN,     KC_K,     KC_L,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
  //\---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------/
                                    LALTMHEN,  LCTLDEL, KC_LOWER,   KC_SPC,        KC_ENT, KC_RAISE, RCTLBSPC, RALTHENK
  //                              \---------+---------+---------+---------/    \---------+---------+---------+---------/
@@ -36,11 +51,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_LOWER] = LAYOUT(
  //.---------+---------+---------+---------+---------+---------\                        /---------+---------+---------+---------+---------+---------.
-       KC_ESC,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,                           KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSLS,
+       KC_ESC,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,                           KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,   KC_DEL,
  //|---------+---------+---------+---------+---------+---------+---------+    +---------+---------+---------+---------+---------+---------+---------|
-      _______,  _______,  _______,  _______,  _______,  _______,  _______,       _______,  _______,  KC_MINS,   KC_EQL,  KC_LCBR,  KC_RCBR,   KC_GRV,
+      _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,       _______,   KC_GRV,  KC_MINS,   KC_EQL,  KC_LCBR,  KC_RCBR,   KC_GRV,
  //|---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------|
-      _______,  _______,  _______,  _______,  _______,  _______,  _______,       _______,  _______,  KC_UNDS,  KC_PLUS,  KC_LBRC,  KC_RBRC,  KC_TILD,
+      _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,       _______,  KC_TILD,  KC_UNDS,  KC_PLUS,  KC_LBRC,  KC_RBRC,  KC_TILD,
  //\---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------/
                                     _______,  _______,  _______,  _______,       _______,  _______,  _______,  _______
  //                              \---------+---------+---------+---------/    \---------+---------+---------+---------/
@@ -52,7 +67,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //|---------+---------+---------+---------+---------+---------+---------+    +---------+---------+---------+---------+---------+---------+---------|
       _______,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,   KC_F11,        KC_F12,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  _______,
  //|---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------|
-      _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,       _______,  KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  _______,  _______,
+      _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,       KC_LGUI,  KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  _______,  _______,
  //\---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------/
                                     _______,  _______,  _______,  _______,       _______,  _______,  _______,  _______
  //                              \---------+---------+---------+---------/    \---------+---------+---------+---------/
@@ -60,7 +75,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_ADJUST] = LAYOUT(
  //.---------+---------+---------+---------+---------+---------\                        /---------+---------+---------+---------+---------+---------.
-        RESET,  AD_WO_L,  ADV_ID1,  ADV_ID2,  ADV_ID3,  ADV_ID4,                           XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        RESET,  AD_WO_L,  ADV_ID1,  ADV_ID2,  ADV_ID3,  ADV_ID4,                            QWERTY,  WORKMAN,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
  //|---------+---------+---------+---------+---------+---------+---------+    +---------+---------+---------+---------+---------+---------+---------|
       XXXXXXX,  DELBNDS,  DEL_ID1,  DEL_ID2,  DEL_ID3,  DEL_ID4,  XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
  //|---------+---------+---------+---------+---------+---------+---------|    |---------+---------+---------+---------+---------+---------+---------|
@@ -80,6 +95,22 @@ layer_state_t layer_state_set_user(const layer_state_t state) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_ble(keycode, record)) {
         return false;
+    }
+    if (record->event.pressed) {
+        switch (keycode) {
+            case KC_QWERTY:
+                {
+                    set_single_persistent_default_layer(LAYER_QWERTY);
+                    return false;
+                }
+                break;
+            case KC_WORKMAN:
+                {
+                    set_single_persistent_default_layer(LAYER_WORKMAN);
+                    return false;
+                }
+                break;
+        }
     }
     return true;
 }
